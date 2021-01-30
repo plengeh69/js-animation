6f2eb0bcf1c5ad2c2f5d3a126aff7e1640e8c90d
Plengeh69 <63069639+plengeh69@users.noreply.github.com>
2021-01-31 05:45:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+      
+for (var i = 1; i < 400; i++){
+  banner.innerHTML += "<div class='blocks'></div>";
+  const duration = Math.random() * 5;
+  blocks[i].style.animationDuration = duration+'s';
+}
+      
+const section = document.querySelector('section');
+setTimeout(function(){
+  section.classList.add('active')
+},14000);
